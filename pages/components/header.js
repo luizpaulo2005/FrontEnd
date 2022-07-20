29582586b0957f4894c0ea664e0a045d55ed9b3a
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2022-07-20 17:15:20 -0300

    Alterações na Header

--- pages/components/header.js
+++ pages/components/header.js
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export default function Header(){
     return(
-        <nav class="navbar border">
-        <span class="btn btn-success">Biblioteca Digital de Pesquisas - IFMS</span>
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item btn btn-success">Página Inicial</li>
+        <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+        <Link href="/"><a class="navbar-brand">Biblioteca Digital de Pesquisas - IFMS</a></Link>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-        <Link href="/posts/tccs"><a class="btn btn-success">Ver Pesquisas</a></Link>
+        <Link href="/posts/tccs"><a class="nav-link">Ver Pesquisas</a></Link>
           </li>
+          
           </ul>
+          </div>
       </nav>
     )
 }
